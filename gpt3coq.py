4fcb9e73ec57daf4c2515f639bc511308de7b1aa
Gurkenglas <gurkenglas@hotmail.de>
2020-06-28 02:15:47 +0200

    Truncate prompts, extract text from completion

--- gpt3coq.py
+++ gpt3coq.py
@@ -3,20 +3,22 @@ import sys, pexpect
 coqtop = pexpect.spawn("coqtop -load-vernac-source " + sys.argv[1])
 
 coqtop.expect("\n\w* < ")
-if coqtop.before.find("Error".encode()) == -1:
-  sys.stderr.write("module didn't compile")
+if coqtop.before.find("Error".encode()) > -1:
+  sys.stderr.write("Module didn't compile.\n")
 
 with open (sys.argv[1], "r") as promptfile:
   prompt = promptfile.read()
-
+  
 import openai
 
 while(True):
-  line = repr(openai.Completion.create(model="engine", prompt=prompt, stop=".", temperature=0, max_tokens=300))
+  if prompt.length > 3000:
+    prompt = prompt[prompt.length-3000:]
+  line = openai.Completion.create(model="engine", prompt=prompt, stop=".", temperature=0, max_tokens=300)['choices'][0]['text']
   coqtop.sendline(line)
   coqtop.expect("\n\w* < ")
   if coqtop.before.find("Error".encode()) == -1:
-    print(line)
-    prompt += line + "\n"
+    sys.stdout.write(line)
+    prompt += line
   else:
-    sys.stderr.write("Tried: " + line)
+    sys.stderr.write("Tried: " + line + "\n")
