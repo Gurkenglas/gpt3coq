23886099908952e2f1b9984f20edac8f2502f8d3
Gurkenglas <gurkenglas@hotmail.de>
2020-06-28 13:03:15 +0200

    add argparse, logging and top_p

--- gpt3coq.py
+++ gpt3coq.py
@@ -1,23 +1,33 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Make GPT-3 complete Coq files.')
+parser.add_argument('--top_p', dest='top_p', type=float, default=0.9)
+parser.add_argument('file', type=str, help='the file to complete')
+
+args = parser.parse_args()
+print(args.top_p)
+print(args.file)
+
 import sys, pexpect
 
-coqtop = pexpect.spawn("coqtop -load-vernac-source " + sys.argv[1])
+coqtop = pexpect.spawn("coqtop -load-vernac-source " + args.file)
 
 coqtop.expect("\n\w* < ")
 if coqtop.before.find("Error".encode()) > -1:
   sys.stderr.write("Module didn't compile.\n")
 
-with open (sys.argv[1], "r") as promptfile:
+with open (args.file, "r") as promptfile:
   prompt = promptfile.read()
 
 import openai
 
 while(True):
   prompt = prompt[-3000:]
-  line = openai.Completion.create(model="engine", prompt=prompt, stop=".", temperature=1, max_tokens=300)['choices'][0]['text'] + "."
+  line = openai.Completion.create(model="engine", prompt=prompt, stop=".", top_p=args.top_p, max_tokens=50)['choices'][0]['text'] + "."
+  sys.stderr.write("Trying: " + line + "\n")
   coqtop.sendline(line)
   coqtop.expect("\n\w* < ")
+  sys.stderr.write("Reply: " + coqtop.before.decode() + "\n")
   if coqtop.before.find("Error".encode()) == -1:
     sys.stdout.write(line)
     prompt += line
-  else:
-    sys.stderr.write("Tried: " + line + "\n")
